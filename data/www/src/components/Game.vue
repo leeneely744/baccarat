460fe8c998d54eb41e4f123b9f39ba8f00658861
green bag <nlee9337@gmail.com>
2019-12-27 22:13:14 +0900

    メイン画面を各コンポーネント置き場に分割

--- data/www/src/components/Game.vue
+++ data/www/src/components/Game.vue
@@ -1,6 +1,8 @@
 <template lang='pug'>
-  div
-    p It is Game board.
+  div#game-panel
+    div#game-board-panel This is game bord panel.
+    div#ruled-line-panel This is ruled line area.
+    div#info-panel This is information panel.
 </template>
 
 <script>
@@ -8,5 +10,25 @@ console.log('Hello')
 </script>
 
 <style>
+#game-panel {
+  display: grid;
+  grid-template-areas:
+    'game-board-area ruled-line-area'
+    'game-board-area info-area';
+}
 
+#game-board-panel {
+  grid-area: game-board-area;
+  background-color: #95EC00;
+}
+
+#ruled-line-panel {
+  grid-area: ruled-line-area;
+  background-color: #B2F63D;
+}
+
+#info-panel {
+  grid-area: info-area;
+  background-color: #C4F66F;
+}
 </style>
