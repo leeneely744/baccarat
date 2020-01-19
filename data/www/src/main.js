0ad5c0976f72626909960f4f40f3bb1db3df9189
green bag <nlee9337@gmail.com>
2020-01-19 20:10:20 +0900

    divide store file

--- data/www/src/main.js
+++ data/www/src/main.js
@@ -1,28 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import Vuex from 'vuex'
 import App from './App'
 import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
-Vue.use(Vuex)
-
-const store = new Vuex.Store({
-  state: {
-    coin: 0
-  },
-  mutations: {
-    increment (state, amount) {
-      state.coin += amount
-    }
-  }
-})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
