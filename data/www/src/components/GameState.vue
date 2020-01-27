969babb4b22e7a2660d63816b0368679e4e426c3
green bag <nlee9337@gmail.com>
2020-01-27 22:10:17 +0900

    mapStateを修正

--- data/www/src/components/GameState.vue
+++ data/www/src/components/GameState.vue
@@ -22,12 +22,14 @@ import { mapState, mapMutations } from 'vuex'
 
 export default {
   name: 'game-state',
-  computed: mapState({
-    usedMoney: state => state.usedMoney,
-    tip1000: state => state.tip.tip1000,
-    tip5000: state => state.tip.tip5000,
-    tip10000: state => state.tip.tip10000
-  }),
+  computed: {
+    ...mapState([
+      'usedMoney',
+      'tip1000',
+      'tip5000',
+      'tip10000'
+    ])
+  },
   methods: {
     ...mapMutations([
       'addTip'
