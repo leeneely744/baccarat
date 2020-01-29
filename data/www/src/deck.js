81499266a04ba48b2d1b4148859e08259ebde0db
green bag <nlee9337@gmail.com>
2020-01-29 17:45:41 +0900

    カードを一枚引くメソッドを追加

--- data/www/src/deck.js
+++ data/www/src/deck.js
@@ -3,6 +3,33 @@
 import _ from 'lodash'
 
 /**
+ * 渡されたデッキから、任意の枚数のカードを引く
+ * @param {引く対象のデッキ} deck Object
+ * @returns {{'絵柄': '数字'}, カードを引いた後のデッキ}
+ */
+export let drawCardFromDeck = (deck = {}) => {
+  let keys = Object.keys(deck)
+  let keysNum = keys.length()
+
+  if (keysNum === 0) { return null }
+
+  let targetPic = keys[Math.floor(Math.random() * Math.floor(keysNum))]
+  let cards = Object.keys(deck[targetPic])
+  let drawedNum = cards.length()
+  let targetCardNum = cards[Math.floor(Math.random() * Math.floor(drawedNum))]
+
+  let result = {[targetPic]: targetCardNum}
+  delete deck[targetPic][targetCardNum]
+
+  // もし特定の絵柄に属するカードが全てなくなったら、絵柄を削除する
+  if (Object.keys(deck[targetPic]).length() === 0) {
+    delete deck[targetPic]
+  }
+
+  return {result, deck}
+}
+
+/**
  * デッキを初期化する関数
  */
 export let initDeck = (deck) => {
