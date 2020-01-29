43e4b61c6df87e147c6995f918044484620322c6
green bag <nlee9337@gmail.com>
2020-01-29 13:02:13 +0900

    デッキユーティリティを作成

---
+++ data/www/src/deck.js
@@ -0,0 +1,79 @@
+// カードデッキに関する設定などを書く
+
+import _ from 'lodash'
+
+/**
+ * デッキを初期化する関数
+ */
+export let initDeck = (deck) => {
+  return _.forEach(deck, function (pic) {
+    pic.fill(false)
+  })
+}
+
+/**
+ * デッキ
+ * falseだと「まだ引かれてない」を表す
+ */
+export let deck = {
+  'club': {
+    '0': false,
+    '1': false,
+    '2': false,
+    '3': false,
+    '4': false,
+    '5': false,
+    '6': false,
+    '7': false,
+    '8': false,
+    '9': false,
+    '10': false,
+    '11': false,
+    '12': false
+  },
+  'diamond': {
+    '0': false,
+    '1': false,
+    '2': false,
+    '3': false,
+    '4': false,
+    '5': false,
+    '6': false,
+    '7': false,
+    '8': false,
+    '9': false,
+    '10': false,
+    '11': false,
+    '12': false
+  },
+  'heart': {
+    '0': false,
+    '1': false,
+    '2': false,
+    '3': false,
+    '4': false,
+    '5': false,
+    '6': false,
+    '7': false,
+    '8': false,
+    '9': false,
+    '10': false,
+    '11': false,
+    '12': false
+  },
+  'spade': {
+    '0': false,
+    '1': false,
+    '2': false,
+    '3': false,
+    '4': false,
+    '5': false,
+    '6': false,
+    '7': false,
+    '8': false,
+    '9': false,
+    '10': false,
+    '11': false,
+    '12': false
+  }
+}
