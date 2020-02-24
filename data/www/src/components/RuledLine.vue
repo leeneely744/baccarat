c7e0168242dcc6587687e0780dbc7ce2a1add3fb
green bag <nlee9337@gmail.com>
2020-02-24 22:35:11 +0900

    罫線盤作成

--- data/www/src/components/RuledLine.vue
+++ data/www/src/components/RuledLine.vue
@@ -1,15 +1,53 @@
 <template>
   <div>
-    This is ruled-line component
+    <div id='ruled-line-base'>
+      <div v-for="x in width" :key=x>
+        <div v-for="y in height" :key=y>
+          <div class="square"/>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+const RULED_LINE_WIDTH = 25
+const RULED_LINE_HEIGHT = 16
+
 export default {
-  name: 'ruled-line'
+  name: 'ruled-line',
+  data: function () {
+    return {
+      width: RULED_LINE_WIDTH,
+      height: RULED_LINE_HEIGHT
+    }
+  }
 }
 </script>
 
 <style>
+/*
+ borderとpaddingをボックスサイズ（widhtとheight）の中に含めて
+ 算出してくれるようになるので、スタイリングがしやすくなる。
+*/
+* {
+  box-sizing: border-box;
+}
 
+#ruled-line-base {
+  background-color: #c0c0c0;
+  width: calc(202px / RULED_LINE_WIDTH);
+  height: calc(202px / RULED_LINE_HEIGHT);
+  display: flex;
+  flex-wrap: wrap;
+  border: solid 1px #333;
+}
+
+.square {
+  background-color: #ffffff;
+  position: relative;
+  width: 16px;
+  height: 16px;
+  border: solid 1px #333;
+}
 </style>
